79e19c34eff0d0e868b7cbe213ef82520cfcddae
Eleazar Morelli Sangil <etc+github@elemorelli.com>
2026-08-13 00:42:41 -0300

    feat: add asset tree filesystem walker

---
+++ src/server/scan/walkAssetTree.ts
@@ -0,0 +1,43 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+export interface WalkedFile {
+  relativePath: string;
+  size: number;
+  mtimeMs: number;
+}
+
+const walkDir = async (
+  rootDir: string,
+  currentDir: string,
+  results: WalkedFile[],
+): Promise<void> => {
+  const entries = await fs.readdir(currentDir, { withFileTypes: true });
+
+  for (const entry of entries) {
+    const entryPath = path.join(currentDir, entry.name);
+
+    if (entry.isDirectory()) {
+      await walkDir(rootDir, entryPath, results);
+
+      continue;
+    }
+
+    if (!entry.isFile()) {
+      continue;
+    }
+
+    const stat = await fs.stat(entryPath);
+    const relativePath = path.relative(rootDir, entryPath).split(path.sep).join("/");
+
+    results.push({ relativePath, size: stat.size, mtimeMs: stat.mtimeMs });
+  }
+};
+
+export const walkAssetTree = async (rootDir: string): Promise<WalkedFile[]> => {
+  const results: WalkedFile[] = [];
+
+  await walkDir(rootDir, rootDir, results);
+
+  return results;
+};
